d5663488aaa91f9deeae2ba4d8310d2e94d80b43
vwxjs666 <nxrix@proton.me>
2025-09-14 11:59:19 +0330

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,4 @@
 window.onload = () => {
-  document.querySelectorAll("h2").forEach((h) => {
-    h.id = h.textContent.trim().toLowerCase().replace(/\s+/g,"-").replace(/[^a-z0-9\-]/g,"");
-    const b = document.createElement("span");
-    b.style.width = b.style.height = "24px";
-    b.style.marginRight = "6px";
-    b.style.cursor = "pointer";
-    b.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>`;
-    b.addEventListener("click",() => {
-      const l = window.location.href.split("#")[0]+"#"+h.id;
-      const t = document.createElement("input");
-      document.body.appendChild(t);
-      t.value = l;
-      t.select();
-      document.execCommand("copy");
-      document.body.removeChild(t);
-      alert(h.innerText+" Link copied to clipboard");
-    });
-    h.insertBefore(b,h.firstChild);
-  });
 
   const langs = ["en"];
 
@@ -26,10 +7,32 @@ window.onload = () => {
 
   const s = paths.slice(2).join("/");
 
-  const main = document.createElement("a");
-  main.href = `/${paths[0]}/${paths[1]}/`;
-  main.textContent = "Back to Main";
-  footer.appendChild(main);
+  if (paths.slice(2).length>0) {
+    const main = document.createElement("a");
+    main.href = `/${paths[0]}/${paths[1]}/`;
+    main.textContent = "Back to Main";
+    footer.appendChild(main);
+
+    document.querySelectorAll("h2").forEach((h) => {
+      h.id = h.textContent.trim().toLowerCase().replace(/\s+/g,"-").replace(/[^a-z0-9\-]/g,"");
+      const b = document.createElement("span");
+      b.style.width = b.style.height = "24px";
+      b.style.marginRight = "6px";
+      b.style.cursor = "pointer";
+      b.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>`;
+      b.addEventListener("click",() => {
+        const l = window.location.href.split("#")[0]+"#"+h.id;
+        const t = document.createElement("input");
+        document.body.appendChild(t);
+        t.value = l;
+        t.select();
+        document.execCommand("copy");
+        document.body.removeChild(t);
+        alert(h.innerText+" Link copied to clipboard");
+      });
+      h.insertBefore(b,h.firstChild);
+    });
+  }
 
   langs.forEach(l => {
     if (l !== paths[1]) {
