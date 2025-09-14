f92e941cb278536f09facd5fa86253fd56262e7d
vwxjs666 <nxrix@proton.me>
2025-09-14 11:08:59 +0330

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ window.onload = () => {
     const b = document.createElement("span");
     b.style.width = b.style.height = "24px";
     b.style.marginRight = "6px";
+    b.style.cursor = "pointer";
     b.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>`;
     b.addEventListener("click",() => {
       const l = window.location.href.split("#")[0]+"#"+h.id;
