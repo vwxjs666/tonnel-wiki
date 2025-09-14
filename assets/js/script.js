2edd3001fbc1930b02656ed009e7bdf43aa095dd
vwxjs666 <nxrix@proton.me>
2025-09-14 11:18:58 +0330

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ window.onload = () => {
 
   const main = document.createElement("a");
   main.href = `/${paths[0]}/${paths[1]}/`;
-  main.textContent = `${paths[1].toUpperCase()} Main`;
+  main.textContent = "Back";
   footer.appendChild(main);
 
   langs.forEach(l => {
