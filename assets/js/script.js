87b58c6ac871913ae9bc1529616838d8944d1984
vwxjs666 <nxrix@proton.me>
2025-09-14 10:33:01 +0330

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-document.querySelectorAll("h2").forEach((h) => {
-  h.id = h.textContent.trim().toLowerCase().replace(/\s+/g,"-").replace(/[^a-z0-9\-]/g,"");
-  const b = document.createElement("span");
-  b.style.width = b.style.height = "24px";
-  b.style.padding = "0 8px";
-  b.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>`;
-  b.addEventListener("click",() => {
-    const l = window.location.href.split("#")[0]+"#"+h.id;
-    const t = document.createElement("input");
-    document.body.appendChild(t);
-    t.value = l;
-    t.select();
-    document.execCommand("copy");
-    document.body.removeChild(t);
-    alert(h.innerText+" Link copied to clipboard");
+window.onload = () => {
+  document.querySelectorAll("h2").forEach((h) => {
+    h.id = h.textContent.trim().toLowerCase().replace(/\s+/g,"-").replace(/[^a-z0-9\-]/g,"");
+    const b = document.createElement("span");
+    b.style.width = b.style.height = "24px";
+    b.style.padding = "0 8px";
+    b.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>`;
+    b.addEventListener("click",() => {
+      const l = window.location.href.split("#")[0]+"#"+h.id;
+      const t = document.createElement("input");
+      document.body.appendChild(t);
+      t.value = l;
+      t.select();
+      document.execCommand("copy");
+      document.body.removeChild(t);
+      alert(h.innerText+" Link copied to clipboard");
+    });
+    h.insertBefore(b,h.firstChild);
   });
-  h.insertBefore(b,h.firstChild);
-});
+}
