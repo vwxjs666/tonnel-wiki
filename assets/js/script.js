9e1dea2214af524a76794329e826f67a0b671112
vwxjs666 <nxrix@proton.me>
2025-09-21 13:20:24 +0330

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,16 +15,16 @@ window.onload = () => {
 
     document.querySelectorAll("h1").forEach((h) => {
       const b = document.createElement("span");
-      b.style.width = b.style.height = "24px";
+      b.style.width = b.style.height = "34px";
       if (paths[1]=="fa") {
-        b.style.marginRight = "6px";
+        b.style.marginLeft = "8px";
       } else {
-        b.style.marginLeft = "6px";
+        b.style.marginRight = "8px";
       }
       b.style.cursor = "pointer";
       b.innerHTML = (paths[1]=="fa")
-        ? `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>`
-        : `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>`;
+        ? `<svg xmlns="http://www.w3.org/2000/svg" width="34px" height="34px" viewBox="0 -960 960 960" fill="currentColor"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>`
+        : `<svg xmlns="http://www.w3.org/2000/svg" width="34px" height="34px" viewBox="0 -960 960 960" fill="currentColor"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>`;
 
       b.addEventListener("click", () => {
         // Copy current paths and remove only the last segment after lang
@@ -40,9 +40,9 @@ window.onload = () => {
       const b = document.createElement("span");
       b.style.width = b.style.height = "24px";
       if (paths[1]=="fa") {
-        b.style.marginRight = "6px";
-      } else {
         b.style.marginLeft = "6px";
+      } else {
+        b.style.marginRight = "6px";
       }
       b.style.cursor = "pointer";
       b.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z"/></svg>`;
